ad72ad1c1f8271b5dee42f725dcef8e2ebfb1451
Jakub Chlanda <jakub@codeplay.com>
2023-10-12 11:08:38 +0100

    [SYCL] Handle required wg size attribute in HIP

--- source/common/ur_util.hpp
+++ source/common/ur_util.hpp
@@ -293,4 +293,13 @@ inline ur_result_t exceptionToResult(std::exception_ptr eptr) {
     }
 }
 
+inline std::pair<std::string, std::string>
+splitMetadataName(const std::string &metadataName) {
+    size_t splitPos = metadataName.rfind('@');
+    if (splitPos == std::string::npos) {
+        return std::make_pair(metadataName, std::string{});
+    }
+    return std::make_pair(metadataName.substr(0, splitPos),
+                          metadataName.substr(splitPos, metadataName.length()));
+}
 #endif /* UR_UTIL_H */
